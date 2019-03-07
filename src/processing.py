4848062a2e3423e42f32bd00670a91b1a7a8ed15
Dev-Khan <cww904@gmail.com>
2019-03-07 11:59:15 +0000

    Issue with cv2 and KDE

--- src/processing.py
+++ src/processing.py
@@ -1,1 +1,36 @@
 import cv2 
+import numpy as np
+from PIL import Image
+
+INPUT = '../Videos/WaveStock.mp4' #Webcam number or filepath to video
+IMG_TEMP_PATH = '~tmp.png'
+
+capture = cv2.VideoCapture(INPUT)
+
+#capture.open()
+print(capture.isOpened())
+
+while capture.isOpened():
+	# Capture frame-by-frame
+	ret, frame = capture.read()
+	
+	# Operate on the frame
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	
+	# Save the new frame as a temp file
+	cv2.imwrite(IMG_TEMP_PATH, frame)
+	
+	# Load the image with PIL for further processing
+	pFrame = Image.open(IMG_TEMP_PATH)
+	#pFrame.size(240, 240)
+	
+	# Display the new frame
+	cv2.imshow('frame', gray) # THIS DOES NOT WORK ON SYSTEMS WITH KDE AS THE ENVIRONMENT
+	
+	if cv2.waitKey(0) & 0xFF == ord('q'):
+		break
+
+# Loop completed, release the capture
+capture.release()
+cv2.destroyAllWindows()
+
